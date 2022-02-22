b5284e3328b0ae28fe7d2febdd365285d0a42f4b
mycallCampbell <mycallcampbell@gmail.com>
2022-02-22 14:55:07 +0000

    dependencies

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns  = [
     path('',views.getRoutes, name="routes"), 
+    path('watches/', views.getProductWatches, name='product-wathces'),
+    path('watch/<str:pk>', views.getWatch, name='watch'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializer import ProductsSerializer
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+
 
 # Create your views here.
 
 def getRoutes(request):
-    return JsonResponse('Hello', safe=False)
+    return JsonResponse('Rob Diamond API', safe=False)
+
+@api_view(['GET'])
+def getProductWatches(request):
+    products = Product.objects.filter(category__contains='watches')
+    serializer = ProductsSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getWatch(request, pk):
+    product = Product.objects.get(_id=pk)
+    serializer = ProductsSerializer(product, many=False)
+    return Response(serializer.data)
