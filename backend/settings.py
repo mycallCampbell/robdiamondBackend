9081abf3f28287f307b9fa8423cd080a8b079a7e
mycallCampbell <mycallcampbell@gmail.com>
2022-02-22 15:35:35 +0000

    added ip address

--- backend/settings.py
+++ backend/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure--7u%3ghk1-e!8%d@7b8+by0=yb#5d0a^ptu&j9$g55t9#qc73(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '139.59.170.75']
 
 
 # Application definition
