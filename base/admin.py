e5caf31fd676a7f8669075fe2b9c8a8ff6f30f98
unknown <mycallcampbell@gmail.com>
2023-03-22 15:22:44 +0000

    added blog model to admin

--- base/admin.py
+++ base/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import * 
+from .models import *
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Blog)
 admin.site.register(Review)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
 admin.site.register(SubscriberList)
-
