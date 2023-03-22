5ee8938563f17bc797870ec3bf9b871c82d9a27e
unknown <mycallcampbell@gmail.com>
2023-03-22 13:18:55 +0000

    added description1 to models

--- base/models.py
+++ base/models.py
@@ -4,6 +4,7 @@ from django.db.models.fields import AutoField
 
 # Create your models here.
 
+
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
@@ -39,16 +40,20 @@ class Product(models.Model):
     videoWidth = models.IntegerField(null=True, blank=True, default=0)
     videoHeight = models.IntegerField(null=True, blank=True, default=0)
     description = models.TextField(null=True, blank=True)
+    description1 = models.TextField(null=True, blank=True)
     descriptionSmall = models.TextField(null=True, blank=True)
-    rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
     numReviews = models.IntegerField(null=True, blank=True, default=0)
     price = models.IntegerField(null=True, blank=True, default=0)
     countInStock = models.IntegerField(null=True, blank=True, default=0)
     createAt = models.DateTimeField(auto_now_add=True)
+    arrangementNumber = models.IntegerField(null=True, blank=True, default=0)
     _id = models.AutoField(primary_key=True, editable=False)
 
     def __str__(self):
         return self.name
+
 
 class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
@@ -60,6 +65,7 @@ class Review(models.Model):
 
     def __str__(self):
         return str(self.rating)
+
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
@@ -76,7 +82,8 @@ class Order(models.Model):
     isPaid = models.BooleanField(default=False)
     paidAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
     isDelivered = models.BooleanField(default=False)
-    deliveredAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    deliveredAt = models.DateTimeField(
+        auto_now_add=False, null=True, blank=True)
     createdAt = models.DateTimeField(auto_now_add=True)
     _id = AutoField(primary_key=True, editable=False)
 
@@ -89,7 +96,8 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
     qty = models.IntegerField(null=True, blank=True, default=0)
-    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
     image = models.CharField(max_length=200, null=True, blank=True)
     size = models.CharField(max_length=100, default='M')
     halfSize = models.BooleanField(default=False)
@@ -100,7 +108,8 @@ class OrderItem(models.Model):
 
 
 class ShippingAddress(models.Model):
-    order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True)
+    order = models.OneToOneField(
+        Order, on_delete=models.CASCADE, null=True, blank=True)
     addressLine1 = models.CharField(max_length=200, null=True, blank=True)
     addressLine2 = models.CharField(max_length=200, null=True, blank=True)
     city = models.CharField(max_length=200, null=True, blank=True)
@@ -122,4 +131,3 @@ class SubscriberList(models.Model):
 
     def __str__(self):
         return str(self.email)
-
